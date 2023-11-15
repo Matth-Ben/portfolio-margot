287c38af8b895af69884269b953cfa2de24732b4
Matth-Ben <matthiasbenoit.pro@gmail.com>
2023-11-15 11:19:58 +0100

    :fire: [GLOBAL] Save json acf

--- wp-content/themes/theme-margot/functions.php
+++ wp-content/themes/theme-margot/functions.php
@@ -24,6 +24,10 @@ if ( !class_exists( 'Timber' ) ) {
 	return;
 }
 
-
 include get_template_directory() . '/custom/index.php';
 
+function acf_export_json( $path ) {
+	$path = get_stylesheet_directory() . '/acf-json';
+	return $path;
+}
+add_filter( 'acf/settings/save_json', 'acf_export_json' );
