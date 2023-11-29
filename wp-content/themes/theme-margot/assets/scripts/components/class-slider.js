e9f9c285bd8702d9343f0e9b7f1c11e9448693c2
Matth-Ben <matthiasbenoit.pro@gmail.com>
2023-11-29 18:19:34 +0100

    :sparkles: [BLOCK-SLIDER] Template + skin + RWD

--- wp-content/themes/theme-margot/assets/scripts/components/class-slider.js
+++ wp-content/themes/theme-margot/assets/scripts/components/class-slider.js
@@ -4,115 +4,82 @@ class Slider
 {
     constructor( element )
     {
-        this.$items = element.querySelectorAll( '.slider--wrapper-item' )
-        this.$numbers = element.querySelectorAll( '.slider--number-item' )
-        this.$titles = element.querySelectorAll( '.slider--title-item' )
+        this.$items = element.querySelectorAll( '.slider--item' )
+        this.$itemsImage = element.querySelectorAll( 'figure' )
         this.$previous = element.querySelector( '.button--before' )
         this.$next = element.querySelector( '.button--after' )
 
-        this.array = []
+        this.totalSlides = this.$items.length
         this.current = 0
-        this.oldCurrent = 0
-        this.direction = 1
+        this.oldSlide = 0
         this.isAnimated = false
+        this.slidesWrap = gsap.utils.wrap(0, this.totalSlides);
 
-        this.$items.forEach((element, i) => {
-            this.array[i] = {
-                'figure': element,
-                'image': element.querySelector('img'),
-                'number': this.$numbers[i],
-                'title': this.$titles[i],
+        this.$items.forEach((item, index) => {
+            if (index === this.current) {
+                this.transitionInSlide(index, 0);
+                return;
             }
+            
+            this.transitionOutSlide(index, 0);
         });
-
-        for (let index = 1; index < this.array.length; index++) {
-            gsap.set(this.array[index]['figure'], {
-                xPercent: -100,
-                duration: 0.6,
-                ease: "power4.out"
-            })
-            gsap.set(this.array[index]['image'], {
-                xPercent: 100,
-                duration: 0.6,
-                ease: "power4.out"
-            })
-            gsap.set([this.array[index]['title'], this.array[index]['number']], {
-                yPercent: 100,
-                opacity: 0,
-                duration: 0.6,
-                ease: "power4.out",
-                stagger: 0.02
-            })
-        }
 
         this.events()
     }
 
     events() {
-        this.$previous.addEventListener('click', () => { if (!this.isAnimated) { this.slideTo(false) } })
-        this.$next.addEventListener('click', () => { if (!this.isAnimated) { this.slideTo(true) } })
+        this.$previous.addEventListener('click', () => { if (!this.isAnimated) { this.handlePrev() } })
+        this.$next.addEventListener('click', () => { if (!this.isAnimated) { this.handleNext() } })
     }
 
-    slideTo(to) {
-        this.isAnimated = true;
-        this.oldCurrent = this.current
+    transitionInSlide(slide, direction = 1, duration = 1) {
+        gsap.fromTo(
+            this.$items[slide],
+            {
+                xPercent: direction > 0 ? 100 : -100
+            },
+            {
+                xPercent: 0,
+                duration
+            }
+        );
+        
+        gsap.fromTo(
+            this.$itemsImage[slide],
+            {
+                xPercent: direction > 0 ? -100 : 100
+            },
+            {
+                xPercent: 0,
+                duration
+            }
+        );
+    }
 
-        if (to) {
-            this.direction = 1
-            this.current = this.current + 1
-        } else {
-            this.direction = -1
-            this.current = this.current - 1
-        }
+    transitionOutSlide(slide, direction = 1, duration = 1) {
+        gsap.to(this.$items[slide], {
+            xPercent: direction > 0 ? -100 : 100,
+            duration
+        });
+        
+        gsap.to(this.$itemsImage[slide], {
+            xPercent: direction > 0 ? 100 : -100,
+            duration
+        });
+    }
 
-        if (this.current < 0) { this.current = this.array.length - 1 }
-        if (this.current > this.array.length - 1) { this.current = 0 }
-
-        gsap.to(this.array[this.oldCurrent]['figure'], {
-            xPercent: 100 * this.direction,
-            duration: 0.6,
-            ease: "power4.out"
-        })
-        gsap.to(this.array[this.oldCurrent]['image'], {
-            xPercent: -100 * this.direction,
-            duration: 0.6,
-            ease: "power4.out"
-        })
-        gsap.to([this.array[this.oldCurrent]['number'], this.array[this.oldCurrent]['title']], {
-            yPercent: 100,
-            opacity: 0,
-            duration: 0.6,
-            ease: "power4.out",
-            stagger: 0.2
-        })
-
-        gsap.fromTo(this.array[this.current]['figure'], {
-            xPercent: -100 * this.direction,
-            duration: 0.6,
-            ease: "power4.out"
-        }, {
-            xPercent: 0,
-            duration: 0.6,
-            ease: "power4.out"
-        })
-        gsap.fromTo(this.array[this.current]['image'], {
-            xPercent: 100 * this.direction,
-            duration: 0.6,
-            ease: "power4.out"
-        }, {
-            xPercent: 0,
-            duration: 0.6,
-            ease: "power4.out"
-        })
-        gsap.to([this.array[this.current]['number'], this.array[this.current]['title']], {
-            yPercent: 0,
-            opacity: 1,
-            duration: 0.6,
-            delay: 0.6,
-            ease: "power4.out",
-            stagger: 0.2,
-            onComplete: () => { this.isAnimated = false }
-        })
+    handlePrev() {
+        this.oldSlide = this.current;
+        this.current = this.slidesWrap(this.current - 1);
+        this.transitionInSlide(this.current, -1);
+        this.transitionOutSlide(this.oldSlide, -1);
+    }
+  
+    handleNext() {
+        this.oldSlide = this.current;
+        this.current = this.slidesWrap(this.current + 1);
+        this.transitionInSlide(this.current);
+        this.transitionOutSlide(this.oldSlide);
     }
 }
 
