e149d7311af86f681f37565a1bd8bb420cc788ba
Matth-Ben <matthiasbenoit.pro@gmail.com>
2023-12-06 21:24:25 +0100

    :dizzy: [SLIDER-REALISATION] Fix animation carousel

--- wp-content/themes/theme-margot/assets/scripts/components/class-slider-realisation.js
+++ wp-content/themes/theme-margot/assets/scripts/components/class-slider-realisation.js
@@ -5,32 +5,69 @@ class SliderRealisation
     constructor( element )
     {
         this.$items = element.querySelectorAll( '.slider-realisation--wrapper-item' )
+        this.$itemsPreview = element.querySelectorAll( '.slider-realisation--preview-item' )
         this.$itemsImage = element.querySelectorAll( 'figure' )
         this.$numbers = element.querySelectorAll( '.slider-realisation--number-item' )
         this.$titles = element.querySelectorAll( '.slider-realisation--title-item' )
         this.$previous = element.querySelector( '.button--before' )
         this.$next = element.querySelector( '.button--after' )
-
+        this.$carousel = element.querySelector( '.slider-realisation--preview' )
+        this.$itemsCarousel = element.querySelectorAll( '.slider-realisation--preview-item' )
+        
         this.totalSlides = this.$items.length
+
         this.current = 0
         this.oldSlide = 0
         this.isAnimated = false
+
         this.slidesWrap = gsap.utils.wrap(0, this.totalSlides);
         this.titlesWrap = gsap.utils.wrap(0, this.$titles.length);
         this.numbersWrap = gsap.utils.wrap(0, this.$numbers.length);
+        this.carouselWrap = gsap.utils.wrap(0, this.$itemsCarousel.length);
 
+        this.init()
+        this.events()
+    }
+
+    init() {
         this.$items.forEach((item, index) => {
             if (index === this.current) {
                 this.transitionInSlide(index, 0);
                 this.transitionInTitle(index);
+                this.transitionInNumber(index);
                 return;
             }
 
             this.transitionOutTitle(index);
             this.transitionOutSlide(index, 0);
+            this.transitionOutNumber(index, 0);
         });
 
-        this.events()
+        gsap.set(this.$itemsCarousel, {
+            scale: (i) => {
+                if (i != 0) {
+                    return 0.75
+                }
+            },
+            x: (i) => {
+                if (i == this.$itemsCarousel.length - 1) {
+                    return -this.$itemsCarousel[0].clientWidth
+                } else {
+                    return i * this.$itemsCarousel[0].clientWidth
+                }
+            },
+            // rotate: (i) => {
+            //     if (i == 0) {
+            //         return 0
+            //     }
+
+            //     if (i == this.$itemsCarousel.length - 1) {
+            //         return -15
+            //     } else {
+            //         return 15
+            //     }
+            // }
+        })
     }
 
     events() {
@@ -51,7 +88,7 @@ class SliderRealisation
         );
         
         gsap.fromTo(
-            this.$itemsImage[slide],
+            this.$items[slide].querySelector('figure'),
             {
                 xPercent: direction > 0 ? -100 : 100
             },
@@ -68,7 +105,7 @@ class SliderRealisation
             duration
         });
         
-        gsap.to(this.$itemsImage[slide], {
+        gsap.to(this.$items[slide].querySelector('figure'), {
             xPercent: direction > 0 ? 100 : -100,
             duration
         });
@@ -76,7 +113,7 @@ class SliderRealisation
 
     transitionInTitle(title) {
         gsap.fromTo(
-            [this.$numbers[title], this.$titles[title]],
+            this.$titles[title],
             {
                 opacity: 0
             },
@@ -89,9 +126,52 @@ class SliderRealisation
     }
 
     transitionOutTitle(title) {
-        gsap.to([this.$numbers[title], this.$titles[title]], {
+        gsap.to(this.$titles[title], {
             opacity: 0,
             duration: 0.6
+        });
+    }
+
+    transitionInNumber(title, direction = 1) {
+        gsap.fromTo(
+            this.$numbers[title],
+            {
+                yPercent: direction > 0 ? -100 : 100
+            },
+            {
+                yPercent: 0,
+                duration: 0.6
+            }
+        );
+    }
+
+    transitionOutNumber(title, direction = 1) {
+        gsap.to(this.$numbers[title], {
+            yPercent: direction > 0 ? 100 : -100,
+            duration: 0.6
+        });
+    }
+
+    transitionInCarousel(slide, direction = 1, duration = 1) {
+        this.$itemsCarousel.forEach((item, i) => {
+            if (i == slide) {
+                gsap.to(item, {
+                    x: direction > 0 ? "-=" + this.$itemsCarousel[0].clientWidth :  "+=" + this.$itemsCarousel[0].clientWidth,
+                    scale: 1,
+                    // rotate: 0,
+                    duration
+                });
+            } else {
+                gsap.to(item, {
+                    x: direction > 0 ? "-=" + this.$itemsCarousel[0].clientWidth :  "+=" + this.$itemsCarousel[0].clientWidth,
+                    scale: 0.75,
+                    // rotate: direction > 0 ? 15 : -15,
+                    duration,
+                    modifiers: {
+                        x: gsap.utils.unitize(gsap.utils.wrap(-this.$itemsCarousel[0].clientWidth * 2, this.$itemsCarousel[0].clientWidth * 4), "px"),
+                    }
+                });
+            }
         });
     }
 
@@ -102,6 +182,9 @@ class SliderRealisation
         this.transitionOutSlide(this.oldSlide, -1);
         this.transitionInTitle(this.current);
         this.transitionOutTitle(this.oldSlide);
+        this.transitionInCarousel(this.current, -1);
+        this.transitionInNumber(this.current);
+        this.transitionOutNumber(this.oldSlide);
     }
   
     handleNext() {
@@ -111,6 +194,10 @@ class SliderRealisation
         this.transitionOutSlide(this.oldSlide);
         this.transitionInTitle(this.current);
         this.transitionOutTitle(this.oldSlide);
+        this.transitionOutTitle(this.oldSlide);
+        this.transitionInCarousel(this.current);
+        this.transitionInNumber(this.current, -1);
+        this.transitionOutNumber(this.oldSlide, -1);
     }
 }
 
