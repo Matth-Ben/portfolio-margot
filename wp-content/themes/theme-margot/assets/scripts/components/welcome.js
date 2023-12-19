6a2f991b6745a65356c502e859a087b744d5cd13
Matth-Ben <matthiasbenoit.pro@gmail.com>
2023-12-19 18:29:31 +0100

    :dizzy: [LOADER] Fix animation

--- wp-content/themes/theme-margot/assets/scripts/components/welcome.js
+++ wp-content/themes/theme-margot/assets/scripts/components/welcome.js
@@ -1,7 +1,6 @@
 export default ( () =>
 {
     document.addEventListener( 'DOMContentLoaded', () => {
-
         if ( document.body.classList.contains( 'welcome' ) ) {
             const welcome = document.querySelector( 'body > .welcome' )
             const text = welcome ? welcome.querySelector( 'p' ) : null
@@ -9,13 +8,13 @@ export default ( () =>
             if ( text ) {
                 setTimeout( () => {
                     text.classList.add( 'is-in-view' )
-                }, 600 )
+                }, 1200 )
                 setTimeout( () => {
                     welcome.classList.add( 'hide' )
-                }, 1200 )
+                }, 1800 )
                 setTimeout( () => {            
                     document.dispatchEvent( new CustomEvent( 'ContentLoaded' ) )
-                }, 2000 )
+                }, 1800 )
             }
         }
     } )
