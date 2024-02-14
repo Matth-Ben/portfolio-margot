8c11647d601e40132599f1a82f25b3715614215c
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-02-14 23:30:46 +0100

    :bug: [GLOBAL] Fix

--- wp-content/themes/theme-margot/assets/scripts/components/class-slider-circle.js
+++ wp-content/themes/theme-margot/assets/scripts/components/class-slider-circle.js
@@ -10,19 +10,29 @@ class SliderCircle
         this.$dots = element.querySelectorAll( '.slider-circle--dot' )
         this.$previous = element.querySelector( '.button--before' )
         this.$next = element.querySelector( '.button--after' )
-        this.$mainCircle = element.querySelector("svg #circlePath");
-        this.$svg = this.$mainCircle.getBBox();
+        this.$mainCircle = element.querySelector(".slider-circle--svg");
+        // this.$svg = this.$mainCircle.getBBox();
         
         this.current = 0
         this.oldSlide = 0
         this.isAnimated = false
         this.arrayItems = []
 
+        this.canUpdate = false
+
+        this.opts = {
+            angle: 0,
+            radius: 0,
+            width: null
+        }
+
         this.$items.forEach((item, i) => {
             this.arrayItems[i] = {
                 'number' : item.querySelector('.slider-circle--number'),
                 'title' : item.querySelector('.slider-circle--title'),
-                'content' : item.querySelector('.slider-circle--content')
+                'content' : item.querySelector('.slider-circle--content'),
+                'image' : this.$imgs[i],
+                'dot' : this.$dots[i],
             }
         });
 
@@ -34,22 +44,56 @@ class SliderCircle
 
     init()
     {
+        const circleHeight = Math.sqrt(Math.pow(this.$mainCircle.offsetWidth, 2))
+        this.opts.radius = circleHeight / 2
+
         for (let i = 1; i < this.$items.length; i++) {
             gsap.set([this.arrayItems[i]['number'], this.arrayItems[i]['title'], this.arrayItems[i]['content']], {
                 yPercent: 100,
                 stagger: 0.02
             })
         }
+        
+        for (let i = 0; i < this.arrayItems.length; i++) {
+            const { width, height } = this.arrayItems[i].image.getBoundingClientRect()
+            const angle = 360 / this.arrayItems.length * i
+    
+            this.transform(this.arrayItems[i].dot, angle, 0, 0)
+            this.transform(this.arrayItems[i].image, angle, width / 2, height / 2)
+    
+            gsap.set(this.arrayItems[i].image, { rotate: 8 })
+        }
 
-        this.positionElements(this.$dots)
-        this.positionElements(this.$imgs)
+        // this.positionElements(this.$dots)
+        // this.positionElements(this.$imgs)
     }
 
     events() {
         this.$previous.addEventListener('click', () => { this.handlePrev() })
         this.$next.addEventListener('click', () => { this.handleNext() })
-        // window.addEventListener('resize', () => { this.init() })
+        window.addEventListener('resize', () => { this.init() })
     }
+
+    transform(el, angle, offsetX = 0, offsetY = 0) {
+        let x, y
+    
+        console.log(this.opts.radius);
+        if (angle <= 90) {
+            x = (Math.cos((90 - angle) * Math.PI / 180) * this.opts.radius) - 10
+            y = this.opts.radius - Math.sqrt(Math.pow(this.opts.radius, 2) - Math.pow(x, 2))
+        } else if (angle > 90 && angle <= 180) {
+            x = (Math.cos((angle - 90) * Math.PI / 180) * this.opts.radius) - 10
+            y = this.opts.radius + Math.sqrt(Math.pow(this.opts.radius, 2) - Math.pow(x, 2))
+        } else if (angle > 180 && angle <= 270) {
+            x = (- Math.cos((270 - angle) * Math.PI / 180) * this.opts.radius) - 10
+            y = this.opts.radius + Math.sqrt(Math.pow(this.opts.radius, 2) - Math.pow(x, 2))
+        } else if (angle > 270) {
+            x = (- Math.cos((angle - 270) * Math.PI / 180) * this.opts.radius) - 10
+            y = this.opts.radius - Math.sqrt(Math.pow(this.opts.radius, 2) - Math.pow(x, 2))
+        }
+    
+        el.style.transform = `translate(${x - offsetX}px, ${y - offsetY}px)`
+    }    
 
     positionElements(items) {
         console.log(this.$svg);
