bf897debb58a88b1e79d2b9460127e6d591b0051
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-03-27 23:47:26 +0100

    :sparkles: [GALLERY-MOZAIK] Template + skin + JS

--- wp-content/themes/theme-margot/assets/scripts/components/class-gallery-mozaik.js
+++ wp-content/themes/theme-margot/assets/scripts/components/class-gallery-mozaik.js
@@ -4,33 +4,94 @@ class CoverRealisations {
     constructor( element )
     {
         this.$images = element.querySelectorAll('.bloc-gallery-mozaik--image')
+        this.$numbers = element.querySelectorAll('.number-item')
+        this.$title = element.querySelector('.title')
+        this.$titles = element.querySelectorAll('.title-item')
+        this.$buttonLeft = element.querySelector('.button--before')
+        this.$buttonRight = element.querySelector('.button--after')
 
-        const numImages = this.$images.length;
-        const angleIncrement = Math.PI / (numImages - 1); // Ajustez l'angle en fonction du nombre d'images
+        this.itemsWrap = gsap.utils.wrap(0, this.$titles.length);
 
-        for (let i = 0; i < numImages; i++) {
-            const angle = angleIncrement * i;
-            const radiusX = element.offsetWidth / 2;
-            const radiusY = element.offsetHeight / 2;
+        this.old = 0
+        this.current = 0
 
-            const x = radiusX * Math.cos(angle) + radiusX; // Ajustez le rayon selon vos besoins
-            const y = radiusY * Math.sin(angle) + radiusY; // Ajustez le rayon selon vos besoins
+        this.init()
+        this.events()
+    }
 
-            // Ajustement pour placer la première image en haut à gauche
-            if (i === 0) {
-                this.$images[i].style.top = `0`;
-                this.$images[i].style.left = `0`;
-            } else {
-                this.$images[i].style.top = `${y - this.$images[i].offsetHeight / 2}px`;
-                this.$images[i].style.left = `${x - this.$images[i].offsetWidth / 2}px`;
-            }
-
-            // Ajustement pour placer l'image à la moitié en bas à droite
-            if (i === Math.floor(numImages / 2)) {
-                this.$images[i].style.top = `${element.offsetHeight - this.$images[i].offsetHeight}px`;
-                this.$images[i].style.left = `${element.offsetWidth - this.$images[i].offsetWidth}px`;
-            }
+    init() {
+        for (let index = 1; index < this.$titles.length; index++) {
+            gsap.set(this.$titles[index], { yPercent: 100 })
+            gsap.set(this.$numbers[index], { yPercent: 100 })
         }
+    }
+
+    events() {
+        this.$images.forEach((image, i) => {
+            image.addEventListener('click', () => {
+                this.old = this.current;
+                this.current = this.itemsWrap(i);
+                this.changeImage()
+                this.changeTitle(1);
+                this.changeNumber(1);
+            })
+        });
+
+        this.$buttonLeft.addEventListener('click', () => { this.handlePrev() })
+        this.$buttonRight.addEventListener('click', () => { this.handleNext() })
+    }
+
+    handlePrev() {
+        this.old = this.current;
+        this.current = this.itemsWrap(this.current - 1);
+        this.changeImage();
+        this.changeTitle(-1);
+        this.changeNumber(-1);
+    }
+  
+    handleNext() {
+        this.old = this.current;
+        this.current = this.itemsWrap(this.current + 1);
+        this.changeImage();
+        this.changeTitle(1);
+        this.changeNumber(1);
+    }
+
+    changeTitle(direction) {
+        gsap.fromTo(this.$titles[this.old], {
+            yPercent: 0
+        }, {
+            yPercent: -100 * direction
+        })
+
+        gsap.to(this.$title, {
+            height: this.$titles[this.current].offsetHeight
+        })
+
+        gsap.fromTo(this.$titles[this.current], {
+            yPercent: 100 * direction
+        }, {
+            yPercent: 0
+        })
+    }
+
+    changeNumber(direction) {
+        gsap.fromTo(this.$numbers[this.old], {
+            yPercent: 0
+        }, {
+            yPercent: -100 * direction
+        })
+
+        gsap.fromTo(this.$numbers[this.current], {
+            yPercent: 100 * direction
+        }, {
+            yPercent: 0
+        })
+    }
+
+    changeImage() {
+        this.$images[this.old].classList.remove('active')
+        this.$images[this.current].classList.add('active')
     }
 }
 
